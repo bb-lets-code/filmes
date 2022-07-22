9f0ca7f4f3a9dc40b5fd8e1b36b2edd05592f2c5
Igor <igor-rh@hotmail.com>
2022-07-22 15:06:42 -0300

    Enviando lista ordenada

--- src/main/java/com/letscode/app/service/BestsMoviesByYearService.java
+++ src/main/java/com/letscode/app/service/BestsMoviesByYearService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import com.letscode.app.model.Movie;
@@ -21,28 +22,35 @@ public class BestsMoviesByYearService {
     public Map<Integer, List<Movie>> execute() {
         this.whiteMovieService = new WriteMovieService();
         // Visualizar os filmes mais bem avaliados por ano
-        Map<Integer, List<Movie>> map = movies.stream().collect(Collectors.groupingBy(Movie::getYear));
-        map.forEach((year,moviesByYear) -> {
-
-            // Ordenar os filmes por rating
-                List<Movie> moviesSorted = moviesByYear.stream().sorted(Comparator.comparing(Movie::getRating).reversed()).collect(Collectors.toList());
-
-                TreeSet<Movie> filmesByYear = new TreeSet<Movie>(Comparator.comparing( f -> {
-                    return f.getRating();
-                }));
+        Map<Integer, List<Movie>> moviesGroupsByYear = movies.stream().collect(Collectors.groupingBy(Movie::getYear));
+        // moviesGroupsByYear.entrySet().stream().map(a -> a.getValue().stream().sorted(Comparator.comparing(Movie::getRating).reversed()).collect(Collectors.toList())).forEach(Collectors.toMap(null, null));
 
 
-                moviesSorted.stream().limit(50).forEach(f->{
-                     filmesByYear.add(f);
-                });
+
+        moviesGroupsByYear
+        .forEach((year,moviesByYear) -> {
+            
+                // Ordenar os filmes por rating
                 
-                // filmesByYear.addAll(moviesByYear.stream().collect(Collectors.toList()));
-                // Set<Movie> filmesByYear = moviesByYear.stream().sorted( Comparator.comparing(Movie::getRating)).limit(50).collect(Collectors.toCollection(TreeSet::new));
+                final TreeSet<Movie> filmesByYear = new TreeSet<Movie>(Comparator.comparing( Movie::getRating).reversed());
+
+                filmesByYear.addAll(
+                    moviesByYear.stream()
+                    .sorted(Comparator.comparing(Movie::getRating).reversed())
+                    .collect(Collectors.toList())
+                );
+
                 whiteMovieService.writeFile(year, filmesByYear);
                 System.out.println("+----------------------------------------------------+");
-            });
+                
+
+        })
+        ;
+        Map<Integer, List<Movie>> moviesSorted = moviesGroupsByYear.entrySet().stream().collect(
+            Collectors.toMap(Entry::getKey, e-> e.getValue().stream().sorted(Comparator.comparing(Movie::getRating).reversed()).limit(50).collect(Collectors.toList()))
+        );
         
-        return map;
+        return moviesSorted;
         
     }
 
