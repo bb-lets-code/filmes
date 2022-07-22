15dafe460e626df3717c478e53d58ad5b853bf02
Igor Rhamon <igor.rh@gmail.com>
2022-07-21 23:14:07 -0300

    Organizando variáveis

--- src/main/java/com/letscode/app/repository/MovieReadRepository.java
+++ src/main/java/com/letscode/app/repository/MovieReadRepository.java
@@ -5,6 +5,7 @@ import com.letscode.app.model.Movie;
 import com.letscode.app.model.Rating;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,38 +23,37 @@ public class MovieReadRepository implements BaseReadModel<Set<Movie>> {
                 .skip(1)
                 .parallel()
                 .map(m ->{
-                    String[] linha = m.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+                        final String[] linha = m.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
 
-                    String [] genre = linha[TableCSV.Genre.ordinal()].startsWith("\"") ?
-                            linha[TableCSV.Genre.ordinal()].substring(1,linha[TableCSV.Genre.ordinal()].length()-1).split(","):
-                            new String[]{linha[TableCSV.Genre.ordinal()]};
-                    String [] actors = linha[TableCSV.Actors.ordinal()].startsWith("\"") ?
-                            linha[TableCSV.Actors.ordinal()].substring(1,linha[TableCSV.Actors.ordinal()].length()-1).split(",\\s|," ):
-                            new String[]{linha[TableCSV.Actors.ordinal()]};
-                    String [] directors = linha[TableCSV.Director.ordinal()].startsWith("\"") ?
-                            linha[TableCSV.Director.ordinal()].substring(1,linha[TableCSV.Director.ordinal()].length()-1).split(",\\s|," ):
-                            new String[]{linha[TableCSV.Director.ordinal()]};
-                    String [] description = linha[TableCSV.Description.ordinal()].startsWith("\"") ?
-                            linha[TableCSV.Description.ordinal()].substring(1,linha[TableCSV.Description.ordinal()].length()-1).split(",\\s|," ):
-                            new String[]{linha[TableCSV.Description.ordinal()]};
+                        final String [] genre = linha[TableCSV.Genre.ordinal()].startsWith("\"") ?
+                                linha[TableCSV.Genre.ordinal()].substring(1,linha[TableCSV.Genre.ordinal()].length()-1).split(","):
+                                new String[]{linha[TableCSV.Genre.ordinal()]};
 
+                        final String [] actors = linha[TableCSV.Actors.ordinal()].startsWith("\"") ?
+                                linha[TableCSV.Actors.ordinal()].substring(1,linha[TableCSV.Actors.ordinal()].length()-1).split(",\\s|," ):
+                                new String[]{linha[TableCSV.Actors.ordinal()]};
 
-                    // Builder para montar o objeto Filme
-                    return new Movie(
-                            linha[TableCSV.Title.ordinal()],
-                            Integer.valueOf(linha[TableCSV.Year.ordinal()]),
-                            genre,
-                            actors,
-                            directors,
-                            description[0],
-                            actors,
-                            0,
-                            new Rating(
-                                    Double.parseDouble(linha[TableCSV.Rating.ordinal()]),
-                                    Integer.parseInt(linha[TableCSV.Votes.ordinal()]))
-                            , null, 0
+                        final String [] directors = linha[TableCSV.Director.ordinal()].startsWith("\"") ?
+                                linha[TableCSV.Director.ordinal()].substring(1,linha[TableCSV.Director.ordinal()].length()-1).split(",\\s|," ):
+                                new String[]{linha[TableCSV.Director.ordinal()]};
 
-                    );
+                        final String description = linha[TableCSV.Description.ordinal()].startsWith("\"") ?
+                                linha[TableCSV.Description.ordinal()].substring(1,linha[TableCSV.Description.ordinal()].length()-1).split(",\\s|," )[0]:
+                                linha[TableCSV.Description.ordinal()];
+
+                        final BigDecimal revenue = BigDecimal.valueOf(Double.valueOf(linha[TableCSV.Revenue.ordinal()]));
+                        final Integer runtime = Integer.valueOf(linha[TableCSV.Runtime.ordinal()]);
+                        final Rating rating = new Rating(
+                                Double.parseDouble(linha[TableCSV.Rating.ordinal()]),
+                                Integer.parseInt(linha[TableCSV.Votes.ordinal()]));
+
+                        final Double metascore = Double.valueOf(linha[TableCSV.Metascore.ordinal()]);
+                        final String title = linha[TableCSV.Title.ordinal()];
+                        final Integer year = Integer.valueOf(linha[TableCSV.Year.ordinal()]);
+                                
+                        // Builder para montar o objeto Filme
+
+                        return new Movie(title,year,genre,actors,directors,description,actors,runtime,rating,revenue,metascore);
                 })
                 .collect(Collectors.toSet());
     }
