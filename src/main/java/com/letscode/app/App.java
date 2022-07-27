549ecfd4d4b62446b7bc6add5339797d57bbcc79
Igor <igor-rh@hotmail.com>
2022-07-27 14:36:04 -0300

    Renaming variables

--- src/main/java/com/letscode/app/App.java
+++ src/main/java/com/letscode/app/App.java
@@ -2,7 +2,9 @@ package com.letscode.app;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Set;
 
+import com.letscode.app.model.Movie;
 import com.letscode.app.repository.MovieRepository;
 import com.letscode.app.service.BestsHorrorMoviesService;
 import com.letscode.app.service.BestsMoviesByYearService;
@@ -16,12 +18,12 @@ public class App {
         InitializationService initializationService = new InitializationService();
         LocalDateTime begin = initializationService.initializeApp();
         MovieRepository repository = new MovieRepository();
-        var test = repository.read();
+        Set<Movie>  movies = repository.read();
 
-        BestsMoviesByYearService bestsMoviesByYearService = new BestsMoviesByYearService(test);
+        BestsMoviesByYearService bestsMoviesByYearService = new BestsMoviesByYearService(movies);
         bestsMoviesByYearService.execute();
 
-        BestsHorrorMoviesService bestsHorrorMoviesService = new BestsHorrorMoviesService(test);
+        BestsHorrorMoviesService bestsHorrorMoviesService = new BestsHorrorMoviesService(movies);
         bestsHorrorMoviesService.execute();
 
         CloseService closeService = new CloseService();
